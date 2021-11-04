4afe28d1bc9209104e34178bcf62bd34708b8b64
maicol <maicol.matteoli@gmail.com>
2021-11-04 23:18:21 +0100

    edit : i found a way better function to do this

--- index.php
+++ index.php
@@ -16,7 +16,35 @@
     "elit"  
   ];
   var_dump($array);
-  
-  
+  $strLorem = "";
+  // $counter = 0;
 
+  // foreach ($array as $key => $element) {
+  //   if (is_array($element)) {
+  //     foreach($element as $key2 => $subElement){
+  //       $strLorem += $subElement;
+  //     }
+  //   }
+  //   else{
+  //     $strLorem += $element;
+  //   }
+  // foreach($array as $key => $element){
+  //   if (is_array($element)) {
+  //     foreach($element as $key2 => $element2){
+  //       if (is_array($element2)) {
+  //         $strLorem .= implode($element2)
+  //       }
+  //     }
+  //     $strLorem .= implode($element) . " "; 
+  //   }
+  //   else{
+  //     $strLorem .= $element;
+  //   }
+  // }
+  function test_print($array, $key){
+    echo "$key holds $array\n";
+  }
+
+  array_walk_recursive($array,'test_print');
+  // var_dump($strLorem);
 ?>
