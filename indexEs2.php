1ff8583f6aeb6072c78fe017f4ea0ad2101d20a3
maicol <maicol.matteoli@gmail.com>
2021-11-05 14:10:09 +0100

    es2 end

--- indexEs2.php
+++ indexEs2.php
@@ -27,9 +27,33 @@ class Student {
   
 }
 
+class SpecialStudent extends Student{
+  public $favoriteColor;
+
+  function __construct($name,$surname,$age,$favoriteColor){
+    $this->name = $name;
+    $this->surname = $surname;
+    $this->age = $age;
+    $this->favoriteColor = $favoriteColor;
+  }
+
+  function get_color(){
+    return $this->favoriteColor;
+  }
+
+}
+
+//create new Student
 $student1 = new Student("franco","pincopallo",14);
 
+//print is info
+echo "name : " . $student1->get_name() . "  |  surname : " . $student1->get_surname() . "\n";
 
-echo "name : " . $student1->get_name() . "  |  surname : " . $student1->get_surname();
+//create new Special Student
+$student2 = new SpecialStudent("gianni","nanni",16,"orange");
+echo " || ";
+//print Special Student info
+echo "name : " . $student2->get_name() . "  |  surname : " . $student2->get_surname() . "  favorite color : " . $student2->get_color();
+
 
 ?>
